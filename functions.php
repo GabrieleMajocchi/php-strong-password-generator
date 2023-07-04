b62c09dfd8e8ebdedf6409384dace605c9a49c70
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-07-04 16:56:02 +0200

    created functions file with the function in it and included it to the index

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php 
+
+function getRandomPassword($length) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomIndex = rand(0, strlen($characters) - 1);
+        $password .= $characters[$randomIndex];
+    }
+    return $password;
+}
+
+?>
--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
-function getRandomPassword($length) {
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
-    $password = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomIndex = rand(0, strlen($characters) - 1);
-        $password .= $characters[$randomIndex];
-    }
-    return $password;
-}
+include_once __DIR__ . "/functions.php";
 
 if (isset($_GET['length'])) {
     $length = intval($_GET['length']);
