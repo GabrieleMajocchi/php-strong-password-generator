cb8b8cbebd6e292e061a9b16421cb99ad939bc89
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-07-04 16:45:52 +0200

    created the index with doctype and form structure

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong password generator</title>
+
+
+</head>
+<body>
+    <h1>Strong password generator:</h1>
+
+    <form method="GET" action="">
+        <label for="length">Length of password:</label>
+        <input type="number" name="length" id="length" value="" placeholder="Minimum 4" required min="4">
+        <input type="submit" value="Generate password">
+    </form>
+
+</body>
+</html>
+
